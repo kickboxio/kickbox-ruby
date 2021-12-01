4d06356c6c358f7394311806a9f4886b991c1620
Lucas Boyd <lucas@allydvm.com>
2021-12-01 10:11:07 -0500

    Add email batch support to ruby api

--- lib/kickbox/api/kickbox.rb
+++ lib/kickbox/api/kickbox.rb
@@ -23,6 +23,35 @@ module Kickbox
         @client.get("/verify?email=#{email}&timeout=#{timeout}", body, options)
       end
 
+      # Email Batch Verification
+      #
+      # '/verify-batch' PUT
+      #
+      # emails - Email addresses to verify
+      def verify_batch(emails, options = {})
+        if options['headers'].blank?
+          options['headers'] = {}
+        end
+
+        date = Time.current.strftime('%m-%d-%y %H-%M')
+        options['headers'].merge!(
+          'Content-Type' => 'text/csv',
+          'X-Kickbox-Filename' => "Batch Email Verification #{date}"
+        )
+
+        emails = emails.join("\n")
+        @client.put("/verify-batch", emails, options)
+      end
+
+      # Email Batch Verification Status
+      #
+      # '/verify-batch/:id' GET
+      #
+      # id - Job ID of the batch verification job
+      def batch_status(id)
+        @client.get("/verify-batch/#{id}")
+      end
+
     end
 
   end
